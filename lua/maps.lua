07a4f617b13108b9f1d84c158844c505fffcfdc5
Bamdad Sabbagh <hi@bamdad.fr>
2023-02-12 14:44:22 +0100

    Remove custom `dw` command

--- lua/maps.lua
+++ lua/maps.lua
@@ -10,7 +10,7 @@ keymap.set('n', '+', '<C-a>')
 keymap.set('n', '-', '<C-x>')
 
 -- Delete a word backwards
-keymap.set('n', 'dw', 'vb"_d')
+--keymap.set('n', 'dw', 'vb"_d')
 
 -- Select all
 keymap.set('n', '<C-a>', 'gg<S-v>G')
