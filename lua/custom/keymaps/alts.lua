36f0ac7e0ab101952404d9695e6488a4d458d086
Bamdad Sabbagh <hi@bamdad.fr>
2023-05-02 18:44:57 +0200

    Improve UI navigation keymaps

--- lua/custom/keymaps/alts.lua
+++ lua/custom/keymaps/alts.lua
@@ -1,19 +1,45 @@
--- alt+s
-vim.keymap.set("n", "<A-s>", "<cmd> w <CR>")
-
 -- alt+w
 vim.keymap.set("n", "<A-w>", function()
   require("nvchad_ui.tabufline").close_buffer()
 end)
 
--- alt+9
-vim.keymap.set("n", "<A-ç>", "<cmd> LazyGit <CR>")
+-- These mappings will be bound to alt and leader keys
+local mappings = {
+  {
+    key = "s",
+    cmd = "w",
+  },
+  {
+    key = "ç",
+    cmd = "LazyGit",
+  },
+  {
+    key = "&",
+    cmd = "NvimTreeToggle",
+  },
+  {
+    key = "$",
+    cmd = "vsp",
+  },
+  {
+    key = "_",
+    cmd = "TodoTrouble",
+  },
+  {
+    key = "é",
+    cmd = "AerialToggle",
+  },
+}
 
--- alt+1
-vim.keymap.set("n", "<A-&>", "<cmd> NvimTreeToggle <CR>")
-
--- alt+$
-vim.keymap.set("n", "<A-$>", "<cmd> vsp <CR>")
-
--- alt+8
-vim.keymap.set("n", "<A-_>", "<cmd> TodoTrouble <CR>")
+for _, mapping in ipairs(mappings) do
+  vim.keymap.set(
+    "n",
+    "<A-" .. mapping.key .. ">",
+    "<cmd> " .. mapping.cmd .. " <CR>"
+  )
+  vim.keymap.set(
+    "n",
+    "<leader>" .. mapping.key,
+    "<cmd> " .. mapping.cmd .. " <CR>"
+  )
+end
