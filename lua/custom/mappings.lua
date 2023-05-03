ae8ef1c15854d943db6811b552f7a80c7748cd8b
Bamdad Sabbagh <hi@bamdad.fr>
2023-05-03 21:50:07 +0200

    Add nowaits to all custom mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -5,13 +5,16 @@ M.disabled = {
   n = {
     ["<leader>w"] = "",
   },
-  v = {},
 }
 
 -- Your custom mappings
 M.abc = {
   n = {
-    ["<leader>o"] = { "<cmd> echo 'test' <CR>", "Test" },
+    ["<leader>o"] = {
+      "<cmd> echo 'test' <CR>",
+      "Test",
+      opts = { nowait = true },
+    },
 
     -- Close buffer
     ["<A-w>"] = {
@@ -19,6 +22,7 @@ M.abc = {
         require("nvchad_ui.tabufline").close_buffer()
       end,
       "Close buffer",
+      opts = { nowait = true },
     },
 
     -- Comment
@@ -27,15 +31,24 @@ M.abc = {
         require("Comment.api").toggle.linewise.current()
       end,
       "Comment",
+      opts = { nowait = true },
     },
 
     -- Write buffer
-    ["<A-s>"] = { "<cmd> w <CR>", "Write buffer" },
-    ["<leader>s"] = { "<cmd> w <CR>", "Write buffer" },
+    ["<A-s>"] = { "<cmd> w <CR>", "Write buffer", opts = { nowait = true } },
+    ["<leader>s"] = { "<cmd> w <CR>", "Write buffer", opts = { nowait = true } },
 
     -- Split vertically
-    ["<A-$>"] = { "<cmd> vsp <CR>", "Split vertically" },
-    ["<leader>$"] = { "<cmd> vsp <CR>", "Split vertically" },
+    ["<A-$>"] = {
+      "<cmd> vsp <CR>",
+      "Split vertically",
+      opts = { nowait = true },
+    },
+    ["<leader>$"] = {
+      "<cmd> vsp <CR>",
+      "Split vertically",
+      opts = { nowait = true },
+    },
 
     -- Key `w`
     ["w"] = { "z", "", opts = { nowait = true } },
@@ -54,45 +67,54 @@ M.abc = {
     ["viZ"] = { "viW", "", opts = { nowait = true } },
 
     -- Number row
-    ["&"] = { "1", "" },
-    ["é"] = { "2", "" },
-    ['"'] = { "3", "" },
-    ["'"] = { "4", "" },
-    ["("] = { "5", "" },
-    ["-"] = { "6", "" },
-    ["è"] = { "7", "" },
-    ["_"] = { "8", "" },
-    ["ç"] = { "9", "" },
-    ["à"] = { "0", "" },
-    [")"] = { "_", "" },
+    ["&"] = { "1", "", opts = { nowait = true } },
+    ["é"] = { "2", "", opts = { nowait = true } },
+    ['"'] = { "3", "", opts = { nowait = true } },
+    ["'"] = { "4", "", opts = { nowait = true } },
+    ["("] = { "5", "", opts = { nowait = true } },
+    ["-"] = { "6", "", opts = { nowait = true } },
+    ["è"] = { "7", "", opts = { nowait = true } },
+    ["_"] = { "8", "", opts = { nowait = true } },
+    ["ç"] = { "9", "", opts = { nowait = true } },
+    ["à"] = { "0", "", opts = { nowait = true } },
+    [")"] = { "_", "", opts = { nowait = true } },
 
     -- Goto Preview
     ["gpd"] = {
       "<cmd> lua require('goto-preview').goto_preview_definition() <CR>",
       "goto_preview_definition",
+      opts = { nowait = true },
     },
     ["gpt"] = {
       "<cmd> lua require('goto-preview').goto_preview_type_definition() <CR>",
       "goto_preview_type_definition",
+      opts = { nowait = true },
     },
     ["gpi"] = {
       "<cmd> lua require('goto-preview').goto_preview_implementation() <CR>",
       "goto_preview_implementation",
+      opts = { nowait = true },
     },
     ["gP"] = {
       "<cmd> lua require('goto-preview').close_all_win() <CR>",
       "close_all_win",
+      opts = { nowait = true },
     },
     ["gpr"] = {
       "<cmd> lua require('goto-preview').goto_preview_references() <CR>",
       "goto_preview_references",
+      opts = { nowait = true },
     },
 
     -- https://github.com/ThePrimeagen/init.lua/blob/master/lua/theprimeagen/remap.lua
-    ["J"] = { "mzJ`z", "Merge next line while maintaining cursor position" },
-    ["<leader>y"] = { [["+y]], "" },
-    ["<leader>Y"] = { [["+Y]], "" },
-    ["<leader>d"] = { [["_d]], "" },
+    ["J"] = {
+      "mzJ`z",
+      "Merge next line while maintaining cursor position",
+      opts = { nowait = true },
+    },
+    ["<leader>y"] = { [["+y]], "", opts = { nowait = true } },
+    ["<leader>Y"] = { [["+Y]], "", opts = { nowait = true } },
+    ["<leader>d"] = { [["_d]], "", opts = { nowait = true } },
   },
 
   v = {
@@ -100,25 +122,42 @@ M.abc = {
     ["<leader>:"] = {
       "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
       "Comment",
+      opts = { nowait = true },
     },
 
     -- https://github.com/ThePrimeagen/init.lua/blob/master/lua/theprimeagen/remap.lua
-    ["J"] = { ":m '>+1<CR>gv=gv", "Move visual block down" },
-    ["K"] = { ":m '<-2<CR>gv=gv", "Move visual block up" },
-    ["<leader>y"] = { [["+y]], "" },
-    ["<leader>d"] = { [["_d]], "" },
+    ["J"] = {
+      ":m '>+1<CR>gv=gv",
+      "Move visual block down",
+      opts = { nowait = true },
+    },
+    ["K"] = {
+      ":m '<-2<CR>gv=gv",
+      "Move visual block up",
+      opts = { nowait = true },
+    },
+    ["<leader>y"] = { [["+y]], "", opts = { nowait = true } },
+    ["<leader>d"] = { [["_d]], "", opts = { nowait = true } },
   },
 
   i = {
-    ["<C-c>"] = { "<Esc>", "" },
+    ["<C-c>"] = { "<Esc>", "", opts = { nowait = true } },
   },
 }
 
 M.NvimTreeToggle = {
   plugin = true,
   n = {
-    ["<A-&>"] = { "<cmd> NvimTreeToggle <CR>", "Toggle tree" },
-    ["<leader>&"] = { "<cmd> NvimTreeToggle <CR>", "Toggle tree" },
+    ["<A-&>"] = {
+      "<cmd> NvimTreeToggle <CR>",
+      "Toggle tree",
+      opts = { nowait = true },
+    },
+    ["<leader>&"] = {
+      "<cmd> NvimTreeToggle <CR>",
+      "Toggle tree",
+      opts = { nowait = true },
+    },
   },
 }
 
@@ -127,8 +166,16 @@ require("core.utils").load_mappings "NvimTreeToggle"
 M.LazyGit = {
   plugin = true,
   n = {
-    ["<A-ç>"] = { "<cmd> LazyGit <CR>", "Open git manager" },
-    ["<leader>ç"] = { "<cmd> LazyGit <CR>", "Open git manager" },
+    ["<A-ç>"] = {
+      "<cmd> LazyGit <CR>",
+      "Open git manager",
+      opts = { nowait = true },
+    },
+    ["<leader>ç"] = {
+      "<cmd> LazyGit <CR>",
+      "Open git manager",
+      opts = { nowait = true },
+    },
   },
 }
 
@@ -137,8 +184,16 @@ require("core.utils").load_mappings "LazyGit"
 M.AerialToggle = {
   plugin = true,
   n = {
-    ["<A-é>"] = { "<cmd> AerialToggle <CR>", "Toggle symbols" },
-    ["<leader>é"] = { "<cmd> AerialToggle <CR>", "Toggle symbols" },
+    ["<A-é>"] = {
+      "<cmd> AerialToggle <CR>",
+      "Toggle symbols",
+      opts = { nowait = true },
+    },
+    ["<leader>é"] = {
+      "<cmd> AerialToggle <CR>",
+      "Toggle symbols",
+      opts = { nowait = true },
+    },
   },
 }
 
@@ -147,8 +202,12 @@ require("core.utils").load_mappings "AerialToggle"
 M.TodoTrouble = {
   plugin = true,
   n = {
-    ["<A-_>"] = { "<cmd> TodoTrouble <CR>", "TODOs" },
-    ["<leader>_"] = { "<cmd> TodoTrouble <CR>", "TODOs" },
+    ["<A-_>"] = { "<cmd> TodoTrouble <CR>", "TODOs", opts = { nowait = true } },
+    ["<leader>_"] = {
+      "<cmd> TodoTrouble <CR>",
+      "TODOs",
+      opts = { nowait = true },
+    },
   },
 }
 
