0f0225e43e4e97938cef12bc91bfd576246a19c3
Bamdad Sabbagh <hi@bamdad.fr>
2023-05-04 16:15:43 +0200

    Add toggle quickfix + Reference options for custom bindings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,4 +1,8 @@
+local toggle_quickfix = require "custom.utils.toggle_quickfix"
+
 local M = {}
+
+local opts = { nowait = true }
 
 -- In order to disable a default keymap, use
 M.disabled = {
@@ -13,7 +17,7 @@ M.abc = {
     ["<leader>o"] = {
       "<cmd> echo 'test' <CR>",
       "Test",
-      opts = { nowait = true },
+      opts,
     },
 
     -- Close buffer
@@ -22,7 +26,14 @@ M.abc = {
         require("nvchad_ui.tabufline").close_buffer()
       end,
       "Close buffer",
-      opts = { nowait = true },
+      opts,
+    },
+
+    -- Close quickfix
+    ['<leader>"'] = {
+      toggle_quickfix,
+      "Close quickfix",
+      opts,
     },
 
     -- Comment
@@ -31,90 +42,117 @@ M.abc = {
         require("Comment.api").toggle.linewise.current()
       end,
       "Comment",
-      opts = { nowait = true },
+      opts,
     },
 
     -- Write buffer
-    ["<A-s>"] = { "<cmd> w <CR>", "Write buffer", opts = { nowait = true } },
-    ["<leader>s"] = { "<cmd> w <CR>", "Write buffer", opts = { nowait = true } },
+    ["<A-s>"] = { "<cmd> w <CR>", "Write buffer", opts },
+    ["<leader>s"] = { "<cmd> w <CR>", "Write buffer", opts },
 
     -- Split vertically
     ["<A-$>"] = {
       "<cmd> vsp <CR>",
       "Split vertically",
-      opts = { nowait = true },
+      opts,
     },
     ["<leader>$"] = {
       "<cmd> vsp <CR>",
       "Split vertically",
-      opts = { nowait = true },
+      opts,
     },
 
     -- Key `w`
-    ["w"] = { "z", "", opts = { nowait = true } },
-    ["<S-w>"] = { "<S-z>", "", opts = { nowait = true } },
-    ["<C-w>"] = { "<C-z>", "", opts = { nowait = true } },
-    ["cw"] = { "cz", "", opts = { nowait = true } },
-    ["dw"] = { "dz", "", opts = { nowait = true } },
+    ["w"] = { "z", "", opts },
+    ["<S-w>"] = { "<S-z>", "", opts },
+    ["<C-w>"] = { "<C-z>", "", opts },
+    ["cw"] = { "cz", "", opts },
+    ["dw"] = { "dz", "", opts },
+    ["ciw"] = { "ciz", "", opts },
 
     -- Key `z`
-    ["z"] = { "w", "", opts = { nowait = true } },
-    ["<C-z>"] = { "<C-w>", "", opts = { nowait = true } },
-    ["<S-z>"] = { "<S-w>", "", opts = { nowait = true } },
-    ["cz"] = { "cw", "", opts = { nowait = true } },
-    ["dz"] = { "dw", "", opts = { nowait = true } },
-    ["viz"] = { "viw", "", opts = { nowait = true } },
-    ["viZ"] = { "viW", "", opts = { nowait = true } },
+    ["z"] = { "w", "", opts },
+    ["<C-z>"] = { "<C-w>", "", opts },
+    ["<S-z>"] = { "<S-w>", "", opts },
+    ["cz"] = { "cw", "", opts },
+    ["dz"] = { "dw", "", opts },
+    ["viz"] = { "viw", "", opts },
+    ["viZ"] = { "viW", "", opts },
+    ["ciz"] = { "ciw", "", opts },
+    ["ciZ"] = { "ciW", "", opts },
 
     -- Number row
-    ["&"] = { "1", "", opts = { nowait = true } },
-    ["é"] = { "2", "", opts = { nowait = true } },
-    ['"'] = { "3", "", opts = { nowait = true } },
-    ["'"] = { "4", "", opts = { nowait = true } },
-    ["("] = { "5", "", opts = { nowait = true } },
-    ["-"] = { "6", "", opts = { nowait = true } },
-    ["è"] = { "7", "", opts = { nowait = true } },
-    ["_"] = { "8", "", opts = { nowait = true } },
-    ["ç"] = { "9", "", opts = { nowait = true } },
-    ["à"] = { "0", "", opts = { nowait = true } },
-    [")"] = { "_", "", opts = { nowait = true } },
+    ["&"] = { "1", "", opts },
+    ["é"] = { "2", "", opts },
+    ['"'] = { "3", "", opts },
+    ["'"] = { "4", "", opts },
+    ["("] = { "5", "", opts },
+    ["-"] = { "6", "", opts },
+    ["è"] = { "7", "", opts },
+    ["_"] = { "8", "", opts },
+    ["ç"] = { "9", "", opts },
+    ["à"] = { "0", "", opts },
+    [")"] = { "_", "", opts },
+
+    -- Jump while keeping cursor at middle row
+    ["<C-d>"] = {
+      "<C-d>zz",
+      "Jump down while keeping cursor at middle row",
+      opts,
+    },
+    ["<C-u>"] = {
+      "<C-u>zz",
+      "Jump up while keeping cursor at middle row",
+      opts,
+    },
+
+    -- Iterate through search results while keeping cursor at middle row
+    ["n"] = {
+      "nzzzv",
+      "Next search result while keeping cursor at middle row",
+      opts,
+    },
+    ["N"] = {
+      "Nzzzv",
+      "Previous search result while keeping cursor at middle row",
+      opts,
+    },
 
     -- Goto Preview
     ["gpd"] = {
       "<cmd> lua require('goto-preview').goto_preview_definition() <CR>",
       "goto_preview_definition",
-      opts = { nowait = true },
+      opts,
     },
     ["gpt"] = {
       "<cmd> lua require('goto-preview').goto_preview_type_definition() <CR>",
       "goto_preview_type_definition",
-      opts = { nowait = true },
+      opts,
     },
     ["gpi"] = {
       "<cmd> lua require('goto-preview').goto_preview_implementation() <CR>",
       "goto_preview_implementation",
-      opts = { nowait = true },
+      opts,
     },
     ["gP"] = {
       "<cmd> lua require('goto-preview').close_all_win() <CR>",
       "close_all_win",
-      opts = { nowait = true },
+      opts,
     },
     ["gpr"] = {
       "<cmd> lua require('goto-preview').goto_preview_references() <CR>",
       "goto_preview_references",
-      opts = { nowait = true },
+      opts,
     },
 
     -- https://github.com/ThePrimeagen/init.lua/blob/master/lua/theprimeagen/remap.lua
     ["J"] = {
       "mzJ`z",
       "Merge next line while maintaining cursor position",
-      opts = { nowait = true },
+      opts,
     },
-    ["<leader>y"] = { [["+y]], "", opts = { nowait = true } },
-    ["<leader>Y"] = { [["+Y]], "", opts = { nowait = true } },
-    ["<leader>d"] = { [["_d]], "", opts = { nowait = true } },
+    ["<leader>y"] = { [["+y]], "", opts },
+    ["<leader>Y"] = { [["+Y]], "", opts },
+    ["<leader>d"] = { [["_d]], "", opts },
   },
 
   v = {
@@ -122,33 +160,38 @@ M.abc = {
     ["<leader>:"] = {
       "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
       "Comment",
-      opts = { nowait = true },
+      opts,
     },
 
     -- https://github.com/ThePrimeagen/init.lua/blob/master/lua/theprimeagen/remap.lua
     ["J"] = {
       ":m '>+1<CR>gv=gv",
       "Move visual block down",
-      opts = { nowait = true },
+      opts,
     },
     ["K"] = {
       ":m '<-2<CR>gv=gv",
       "Move visual block up",
-      opts = { nowait = true },
+      opts,
     },
-    ["<leader>y"] = { [["+y]], "", opts = { nowait = true } },
-    ["<leader>d"] = { [["_d]], "", opts = { nowait = true } },
+    ["<leader>y"] = { [["+y]], "", opts },
+    ["<leader>d"] = { [["_d]], "", opts },
+    ["<leader>p"] = {
+      '"_dP',
+      "Paste over while retaining paste buffer",
+      opts,
+    },
   },
 
   i = {
     -- Exit
-    ["<C-c>"] = { "<Esc>", "", opts = { nowait = true } },
+    ["<C-c>"] = { "<Esc>", "", opts },
 
     -- Key `z`
-    ["<C-z>"] = { "<C-w>", "", opts = { nowait = true } },
+    ["<C-z>"] = { "<C-w>", "", opts },
 
     -- Key `w`
-    ["<C-w>"] = { "<C-z>", "", opts = { nowait = true } },
+    ["<C-w>"] = { "<C-z>", "", opts },
   },
 }
 
@@ -158,12 +201,12 @@ M.NvimTreeToggle = {
     ["<A-&>"] = {
       "<cmd> NvimTreeToggle <CR>",
       "Toggle tree",
-      opts = { nowait = true },
+      opts,
     },
     ["<leader>&"] = {
       "<cmd> NvimTreeToggle <CR>",
       "Toggle tree",
-      opts = { nowait = true },
+      opts,
     },
   },
 }
@@ -176,12 +219,12 @@ M.LazyGit = {
     ["<A-ç>"] = {
       "<cmd> LazyGit <CR>",
       "Open git manager",
-      opts = { nowait = true },
+      opts,
     },
     ["<leader>ç"] = {
       "<cmd> LazyGit <CR>",
       "Open git manager",
-      opts = { nowait = true },
+      opts,
     },
   },
 }
@@ -194,12 +237,12 @@ M.AerialToggle = {
     ["<A-é>"] = {
       "<cmd> AerialToggle <CR>",
       "Toggle symbols",
-      opts = { nowait = true },
+      opts,
     },
     ["<leader>é"] = {
       "<cmd> AerialToggle <CR>",
       "Toggle symbols",
-      opts = { nowait = true },
+      opts,
     },
   },
 }
@@ -209,11 +252,11 @@ require("core.utils").load_mappings "AerialToggle"
 M.TodoTrouble = {
   plugin = true,
   n = {
-    ["<A-_>"] = { "<cmd> TodoTrouble <CR>", "TODOs", opts = { nowait = true } },
+    ["<A-_>"] = { "<cmd> TodoTrouble <CR>", "TODOs", opts },
     ["<leader>_"] = {
       "<cmd> TodoTrouble <CR>",
       "TODOs",
-      opts = { nowait = true },
+      opts,
     },
   },
 }
