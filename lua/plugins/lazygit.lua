4e39713b4e7470dc29900f8ee09850dc251eb93a
Bamdad Sabbagh <hi@bamdad.fr>
2023-07-28 22:00:01 +0200

    Comment not working keybinding for lazygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -2,6 +2,6 @@ return {
   "kdheepak/lazygit.nvim",
   keys = {
     { "<leader>ç", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
-    { "<A-ç>", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
+    -- { "<A-ç>", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
   },
 }
