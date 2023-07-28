011b1d91e673e6793fec84d67cac2af6b06ac8ee
Bamdad Sabbagh <hi@bamdad.fr>
2023-07-28 07:45:39 +0200

    Add comment plugin

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,20 @@
+return {
+  "numToStr/Comment.nvim",
+  keys = {
+    {
+      "<leader>:",
+      function()
+        require("Comment.api").toggle.linewise.current()
+      end,
+    },
+    {
+      "<leader>:",
+      "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
+      desc = "Toggle comment",
+      mode = "v",
+    },
+  },
+  config = function()
+    require("Comment").setup()
+  end,
+}
