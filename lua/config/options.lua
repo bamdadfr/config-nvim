96e0033e1e1580c5a664970bbf8081d5e26438d4
Bamdad Sabbagh <hi@bamdad.fr>
2023-07-30 00:59:09 +0200

    Disable concealing

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Set markdown formatting
 vim.cmd([[set formatexpr=markdownfmt#Format]])
+
+-- Disable concealing
+vim.cmd([[set conceallevel=0]])
