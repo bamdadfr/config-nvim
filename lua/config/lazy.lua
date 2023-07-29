c4343a70ff184a94f1ac6d874e2c708c26c84e0b
Bamdad Sabbagh <hi@bamdad.fr>
2023-07-29 23:30:12 +0200

    Add unit tests with neotest

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.test.core" },
     -- import/override with your plugins
     { import = "plugins" },
   },
