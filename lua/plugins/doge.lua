a2496bec50879b85d1baba680d8611a77b7ce540
Bamdad Sabbagh <hi@bamdad.fr>
2023-07-29 02:33:28 +0200

    Update doge plugin

--- lua/plugins/doge.lua
+++ lua/plugins/doge.lua
@@ -1,12 +1,9 @@
 return {
   "kkoomen/vim-doge",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-  },
+  lazy = false,
   keys = {
     { "<leader>gk", "<cmd> DogeGenerate <cr>", desc = "Generate documentation" },
   },
-  lazy = false,
   config = function()
     vim.cmd([[call doge#install()]])
     vim.cmd([[let g:doge_doc_standard_python = "google"]])
