cbea28bb028897d2547692b6bedbf6d29ec1fb6d
Bamdad Sabbagh <hi@bamdad.fr>
2023-08-01 01:31:32 +0200

    Switch between telescope find files (all and cwd)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -38,10 +38,10 @@ map("n", "<A-k>", "<C-w>k", { desc = "Go to above window", remap = true })
 map("n", "<A-j>", "<C-w>j", { desc = "Go to below window", remap = true })
 
 -- Telescope
-map("n", "<leader>fO", "<cmd> Telescope oldfiles <cr>", { desc = "Find recent files", remap = true })
+map("n", "<leader>fo", "<cmd> Telescope oldfiles <cr>", { desc = "Find recent files (all)", remap = true })
 
 local telescope_oldfiles_cwd = Util.telescope("oldfiles", { cwd = vim.loop.cwd() })
-map("n", "<leader>fo", telescope_oldfiles_cwd, { desc = "Find recent files (cwd)", remap = true })
+map("n", "<leader>fO", telescope_oldfiles_cwd, { desc = "Find recent files (cwd)", remap = true })
 map("n", "<leader>fw", Util.telescope("live_grep"), { desc = "Find grep", remap = true })
 
 -- Number row for azerty
