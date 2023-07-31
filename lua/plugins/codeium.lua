9403549d29000a336ee8074853480a9fb42a68b5
Bamdad Sabbagh <hi@bamdad.fr>
2023-07-31 02:20:26 +0200

    Add codeium

---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,23 @@
+return {
+  "Exafunction/codeium.vim",
+  lazy = false,
+  config = function()
+    vim.g.codeium_disable_bindings = 1
+
+    vim.keymap.set("i", "<C-p>", function()
+      return vim.fn["codeium#Accept"]()
+    end, { expr = true })
+
+    vim.keymap.set("i", "<C-o>", function()
+      return vim.fn["codeium#Clear"]()
+    end, { expr = true })
+
+    vim.keymap.set("i", "<A-=>", function()
+      return vim.fn["codeium#CycleCompletions"](1)
+    end, { expr = true })
+
+    vim.keymap.set("i", "<A-)>", function()
+      return vim.fn["codeium#CycleCompletions"](-1)
+    end, { expr = true })
+  end,
+}
