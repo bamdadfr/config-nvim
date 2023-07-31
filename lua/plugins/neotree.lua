a3b189aa076d135ead415104172df5c52e0c434b
Bamdad Sabbagh <hi@bamdad.fr>
2023-07-31 23:33:15 +0200

    Update neotree plugin configuration

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -2,23 +2,31 @@ local toggle = {
   command = function()
     require("neo-tree.command").execute({
       toggle = true,
-      position = "right",
     })
   end,
   description = "Toggle Neotree",
 }
 
 local focus = {
-  command = "<cmd>Neotree focus right<cr>",
+  command = "<cmd>Neotree focus<cr>",
   description = "Focus Neotree",
 }
 
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  lazy = false,
   keys = {
     { "<A-&>", toggle.command, desc = toggle.description },
     { "<leader>&", toggle.command, desc = toggle.description },
     { "<A-e>", focus.command, desc = focus.description },
     { "<leader>e", focus.command, desc = focus.description },
   },
+  config = function()
+    require("neo-tree").setup({
+      window = {
+        position = "right",
+        width = 50,
+      },
+    })
+  end,
 }
