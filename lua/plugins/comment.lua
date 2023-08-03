bac2264009ceb1486c6683964145380a75808f09
Bamdad Sabbagh <hi@bamdad.fr>
2023-08-03 07:50:15 +0200

    Add remaps to comment keybindings

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -6,12 +6,15 @@ return {
       function()
         require("Comment.api").toggle.linewise.current()
       end,
+      mode = "n",
+      remap = true,
     },
     {
       "<leader>:",
       "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
       desc = "Toggle comment",
       mode = "v",
+      remap = true,
     },
   },
   config = function()
