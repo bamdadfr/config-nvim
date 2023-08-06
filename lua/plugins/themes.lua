e5d6680ff4ac45a0e290ff9a63a00b20a7fb89e2
Bamdad Sabbagh <hi@bamdad.fr>
2023-08-06 15:56:44 +0200

    Merge all themes to single file

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "catppuccin/nvim",
+    config = function()
+      require("catppuccin").setup({
+        flavour = "latte",
+        transparent_background = true,
+      })
+
+      vim.cmd.colorscheme("catppuccin-latte")
+
+      require("notify").setup({
+        background_colour = "#000000",
+      })
+    end,
+  },
+  {
+    "projekt0n/github-nvim-theme",
+  },
+}
