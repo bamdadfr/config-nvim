1cac94089b95b393746df1467b12795d5345843e
Bamdad Sabbagh <hi@bamdad.fr>
2023-11-10 18:05:44 +0100

    Markdown: Remove timeouts

--- lua/plugins/lang-markdown.lua
+++ lua/plugins/lang-markdown.lua
@@ -14,12 +14,11 @@ return {
     "nvimtools/none-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
-
-      table.insert(opts.sources, nls.builtins.code_actions.proselint.with({ timeout = 2000 }))
-      table.insert(opts.sources, nls.builtins.code_actions.ltrs.with({ timeout = 2000 }))
-      table.insert(opts.sources, nls.builtins.diagnostics.alex.with({ timeout = 2000 }))
-      table.insert(opts.sources, nls.builtins.diagnostics.markdownlint.with({ timeout = 2000 }))
-      table.insert(opts.sources, nls.builtins.formatting.markdownlint.with({ timeout = 2000 }))
+      table.insert(opts.sources, nls.builtins.code_actions.proselint)
+      table.insert(opts.sources, nls.builtins.code_actions.ltrs)
+      table.insert(opts.sources, nls.builtins.diagnostics.alex)
+      table.insert(opts.sources, nls.builtins.diagnostics.markdownlint)
+      table.insert(opts.sources, nls.builtins.formatting.markdownlint)
     end,
   },
 }
