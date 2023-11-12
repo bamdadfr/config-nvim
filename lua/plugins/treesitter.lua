50431409a04f13e9d8563211605b6f616a7f47c5
Bamdad Sabbagh <hi@bamdad.fr>
2023-11-12 16:53:34 +0100

    TS: Update mdx TS parser registration

--- lua/plugins/treesitter.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function()
-    local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
-    ft_to_parser.mdx = "markdown"
-  end,
-}
