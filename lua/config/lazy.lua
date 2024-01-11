a71382810fb959f1409312ff7252eba461834118
Bamdad Sabbagh <hi@bamdad.fr>
2024-01-11 04:17:46 +0100

    Plugins: Add rust

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,6 +27,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     -- tailwind
