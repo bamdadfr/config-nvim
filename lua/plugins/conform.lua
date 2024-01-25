bb4467053de88f7fc96427adf7eb85b9a3f52da8
Bamdad Sabbagh <hi@bamdad.fr>
2024-01-25 10:47:20 +0100

    Conform: Replace prettierd with prettier + Improve configuration

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,7 +5,7 @@ return {
       vim.list_extend(opts.ensure_installed, {
         "stylua",
         "markdownlint",
-        "prettierd",
+        "prettier",
         "isort",
         "black",
         "vue-language-server",
@@ -17,35 +17,22 @@ return {
   },
   {
     "stevearc/conform.nvim",
-    dependencies = { "mason.nvim" },
-    lazy = true,
-    cmd = "ConformInfo",
-    opts = function()
-      local conform = require("conform")
-      conform.formatters_by_ft.fish = { "fish_indent" }
-      conform.formatters_by_ft.lua = { "stylua" }
-      conform.formatters_by_ft.markdown = { "markdownlint", "prettierd" }
-
-      -- yaml
-      conform.formatters_by_ft.yaml = { "prettierd", "yamlfix" }
-
-      -- python
-      conform.formatters_by_ft.python = { "isort", "black" }
-
-      -- css
-      conform.formatters_by_ft.css = { "prettierd" }
-
-      -- javascript
-      conform.formatters_by_ft.javascript = { "prettierd" }
-      conform.formatters_by_ft.typescript = { "prettierd" }
-
-      -- vue
-      conform.formatters_by_ft.vue = { "prettierd" }
-
-      -- react
-      conform.formatters_by_ft.javascriptreact = { "prettierd" }
-      conform.formatters_by_ft.typescriptreact = { "prettierd" }
-      conform.formatters_by_ft.mdx = { "markdownlint", "prettierd" }
-    end,
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        fish = { "fish_indent" },
+        lua = { "stylua" },
+        markdown = { "markdownlint", "prettier" },
+        yaml = { "prettier", "yamlfix" },
+        python = { "isort", "black" },
+        css = { "prettier" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        vue = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        mdx = { "markdownlint", "prettier" },
+      },
+    },
   },
 }
