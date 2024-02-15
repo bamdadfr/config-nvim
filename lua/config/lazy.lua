5f14164bfa59c67f5b7408360783a77989126047
Bamdad Sabbagh <hi@bamdad.fr>
2024-02-16 00:21:27 +0100

    Config: Remove tailwind extra config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,8 +30,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    -- tailwind
-    { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
