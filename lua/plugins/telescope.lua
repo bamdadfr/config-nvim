513f8e76e8bf23eefd5c6cc77a8f92737a3822bf
Bamdad Sabbagh <hi@bamdad.fr>
2024-06-12 11:41:37 +0200

    plugin/telescope: update lazyvim util

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,15 +5,15 @@ return {
   keys = {
     -- Grep
     { "<leader>:", false },
-    { "<leader>fw", Util.telescope("live_grep", { cwd = false }), desc = "Find grep", remap = true },
+    { "<leader>fw", Util.pick("live_grep", { cwd = false }), desc = "Find grep", remap = true },
     -- Files
-    { "<leader>ff", Util.telescope("files", { cwd = false }), desc = "Find files (all)", remap = true },
-    { "<leader>fF", Util.telescope("files"), desc = "Find files (cwd)", remap = true },
+    { "<leader>ff", Util.pick("files", { cwd = false }), desc = "Find files (all)", remap = true },
+    { "<leader>fF", Util.pick("files"), desc = "Find files (cwd)", remap = true },
     -- Recent files
     { "<leader>fo", "<cmd> Telescope oldfiles <cr>", desc = "Find recent files (all)", remap = true },
     {
       "<leader>fO",
-      Util.telescope("oldfiles", { cwd = vim.loop.cwd() }),
+      Util.pick("oldfiles", { cwd = vim.loop.cwd() }),
       desc = "Find recent files (cwd)",
       remap = true,
     },
