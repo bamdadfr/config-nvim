8652b1a87f1973ab4c67444fad59c2292a5bc563
Bamdad Sabbagh <hi@bamdad.fr>
2024-10-10 16:55:53 +0200

    fix: jump one line after commenting

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -5,19 +5,17 @@ return {
       "<leader>:",
       function()
         require("Comment.api").toggle.linewise.current()
+        vim.cmd("normal! j")
       end,
       mode = "n",
       remap = true,
     },
     {
       "<leader>:",
-      "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
+      "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>j",
       desc = "Toggle comment",
       mode = "v",
       remap = true,
     },
   },
-  config = function()
-    require("Comment").setup()
-  end,
 }
