9ec462ed71ce5692015b5fb545a2dbf942a24345
Bamdad Sabbagh <hi@bamdad.fr>
2024-12-04 00:17:50 +0100

    plugins: add shortcut for todo telescope search

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -2,6 +2,7 @@ return {
   "folke/todo-comments.nvim",
   keys = {
     { "<leader>st", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Find TODO/FIX/FIXME" },
+    { "<leader>ft", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Find TODO/FIX/FIXME" },
     { "<leader>sT", "<cmd>TodoTelescope<cr>", desc = "Find all TODOs" },
   },
 }
