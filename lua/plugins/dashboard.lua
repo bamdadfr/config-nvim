f56cb223c8ac29e3b8b5349e6cb2a38cbbe23a08
Bamdad Sabbagh <hi@bamdad.fr>
2025-03-17 20:58:24 +0100

    plugins/dashboard: align header to left to keep indentation

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -3,6 +3,9 @@ return {
     "snacks.nvim",
     opts = {
       dashboard = {
+        formats = {
+          header = { "%s", align = "left" },
+        },
         preset = {
           header = require("logos.random_logo"),
         },
