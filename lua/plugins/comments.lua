d50fa090763ff67c841d8533cd194c17086ad5fd
Bamdad Sabbagh <hi@bamdad.fr>
2025-03-19 03:58:32 +0100

    plugins/comments: disable default `<leader>:`

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,19 +1,27 @@
 return {
-  "numToStr/Comment.nvim",
-  keys = {
-    {
-      "<leader>:",
-      function()
-        require("Comment.api").toggle.linewise.current()
-        vim.cmd("normal! j")
-      end,
-      mode = "n",
+  {
+    "folke/snacks.nvim",
+    keys = {
+      { "<leader>:", mode = { "n" }, false },
     },
-    {
-      "<leader>:",
-      "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>j",
-      desc = "Toggle comment",
-      mode = "v",
+  },
+  {
+    "numToStr/Comment.nvim",
+    keys = {
+      {
+        "<leader>:",
+        function()
+          require("Comment.api").toggle.linewise.current()
+          vim.cmd("normal! j")
+        end,
+        mode = "n",
+      },
+      {
+        "<leader>:",
+        "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>j",
+        desc = "Toggle comment",
+        mode = "v",
+      },
     },
   },
 }
