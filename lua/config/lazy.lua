747d5f0ae18f8ef0f56d02b1e6afb1260d6953d4
Bamdad Sabbagh <hi@bamdad.fr>
2025-03-25 02:44:33 +0100

    configg: add prettier

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,7 +26,7 @@ require("lazy").setup({
     -- dap
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
-    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
