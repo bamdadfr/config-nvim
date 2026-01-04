37ce2aee69d0da524b2228b0c12bc4b29e99fc0f
Bamdad Sabbagh <hi@bamdad.fr>
2026-01-04 02:34:33 +0100

    plugins: update obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,58 +1,37 @@
+-- default options: https://github.com/obsidian-nvim/obsidian.nvim/blob/main/lua/obsidian/config/default.lua
 return {
-  {
-    "saghen/blink.compat",
-    version = "*",
-    lazy = true,
-    opts = {
-      impersonate_nvim_cmp = true,
-      debug = true,
+  "obsidian-nvim/obsidian.nvim",
+  version = "*", -- use latest release, remove to use latest commit
+  ft = "markdown",
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    workspaces = {
+      {
+        name = "vault",
+        path = "~/Nextcloud/obsidian/",
+      },
+    },
+    daily_notes = {
+      folder = "notes",
+      date_format = "%Y%m%d-Note",
+      workdays_only = false,
+    },
+    templates = {
+      folder = "templates",
+      date_format = "%Y-%m-%d-%a",
+      time_format = "%H:%M",
+    },
+    ui = {
+      enable = false,
     },
   },
-  {
-
-    "epwalsh/obsidian.nvim",
-    version = "*",
-    lazy = true,
-    ft = "markdown",
-    event = {
-      "BufReadPre " .. vim.fn.expand("~") .. "/Nextcloud/obsidian/*.md",
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-      "nvim-telescope/telescope.nvim",
-    },
-    opts = {
-      dir = "~/Nextcloud/obsidian/",
-      new_notes_location = "current_dir",
-      completion = {
-        nvim_cmp = true,
-        min_chars = 2,
-      },
-      daily_notes = {
-        folder = "notes",
-        date_format = "%Y%m%d-Note-",
-      },
-      templates = {
-        folder = "templates",
-        date_format = "%Y-%m-%d-%a",
-        time_format = "%H:%M",
-      },
-      ui = {
-        enable = false,
-      },
-      mappings = {},
-    },
-    config = function(_, opts)
-      require("obsidian").setup(opts)
-    end,
-    keys = {
-      { "<leader>od", "<cmd> ObsidianFollowLink <cr>", { desc = "Go to Obsidian definition", remap = true } },
-      { "<leader>or", "<cmd> ObsidianBacklinks <cr>", { desc = "Show Obsidian references", remap = true } },
-      { "<leader>of", "<cmd> ObsidianQuickSwitch <cr>", { desc = "Find Obsidian notes", remap = true } },
-      { "<leader>oo", "<cmd> ObsidianToday <cr>", { desc = "Create Obsidian note", remap = true } },
-      { "<leader>ow", "<cmd> ObsidianSearch <cr>", { desc = "Search Obsidian (grep)", remap = true } },
-      { "<leader>ot", "<cmd> ObsidianTemplate <cr>", { desc = "Use Obsidian template", remap = true } },
-    },
+  keys = {
+    { "<leader>od", "<cmd> ObsidianFollowLink <cr>", { desc = "Go to Obsidian definition", remap = true } },
+    { "<leader>or", "<cmd> ObsidianBacklinks <cr>", { desc = "Show Obsidian references", remap = true } },
+    { "<leader>of", "<cmd> ObsidianQuickSwitch <cr>", { desc = "Find Obsidian notes", remap = true } },
+    { "<leader>oo", "<cmd> ObsidianToday <cr>", { desc = "Create Obsidian note", remap = true } },
+    { "<leader>ow", "<cmd> ObsidianSearch <cr>", { desc = "Search Obsidian (grep)", remap = true } },
+    { "<leader>ot", "<cmd> ObsidianTemplate <cr>", { desc = "Use Obsidian template", remap = true } },
   },
 }
