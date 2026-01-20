0c08481d77f849a79e00ee028156f7bd2be752e0
Bamdad Sabbagh <hi@bamdad.fr>
2026-01-20 17:06:35 +0100

    plugins: update todo shortcuts

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -1,8 +1,39 @@
 return {
   "folke/todo-comments.nvim",
+  opts = {
+    keywords = {
+      STALL = { icon = " ", color = "info" },
+    },
+  },
   keys = {
-    { "<leader>st", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Find TODO/FIX/FIXME" },
-    { "<leader>ft", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Find TODO/FIX/FIXME" },
     { "<leader>sT", "<cmd>TodoTelescope<cr>", desc = "Find all TODOs" },
+    {
+      "<leader>st",
+      function()
+        require("todo-comments.fzf").todo({ keywords = { "TODO", "FIX", "FIXME" } })
+      end,
+      desc = "TODO/FIX/FIXME",
+    },
+    {
+      "<leader>sT",
+      function()
+        require("todo-comments.fzf").todo()
+      end,
+      desc = "ALL TODOS",
+    },
+    {
+      "<leader>ss",
+      function()
+        require("todo-comments.fzf").todo({ keywords = { "STALL" } })
+      end,
+      desc = "STALL",
+    },
+    {
+      "<leader>si",
+      function()
+        require("todo-comments.fzf").todo({ keywords = { "INFO" } })
+      end,
+      desc = "INFO",
+    },
   },
 }
